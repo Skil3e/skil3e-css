f16bb2ec7d8a3140ed7710e8bd98d0050066ddbf
Skil3e <manosm.design@gmail.com>
2020-09-20 15:06:53 +0300

    Version 3.0.0

---
+++ docs/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Logo(props) {
+    return (
+        <h2>Skil3e</h2>
+    );
+}
--- docs/styles-a86e9effd9c7117cc116.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[26],[]]);
-//# sourceMappingURL=styles-a86e9effd9c7117cc116.js.map
