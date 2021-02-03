17e20940121fb2b0127586e41ef457962ebea606
Skil3e <manosm.design@gmail.com>
2021-02-03 15:19:59 +0200

    3.0.0-RC-6.5 Creating Documentation & Refactoring

--- docs/src/components/layout/Header.tsx
+++ docs/src/components/layout/Header.tsx
@@ -3,22 +3,23 @@ import { FC } from "react";
 import Logo from "../Logo";
 import { Link } from "gatsby";
 import ThemeChanger from "./ThemeChanger";
+import Github from "../Github";
 
 interface IHeader {
     className?: string
 }
 
 const Header: FC<IHeader> = ( { className } ) => {
-
     return (
         <header className={ className }>
             <nav className={ "container flex--center w--100 px--lg" }>
                 <Link className={ "flex--center text--text" } to={ "/" }>
                     <Logo className={ "mr--md" }/>
-                    <span className={ "h3 mr--10 text--textDimmed" }>skil3e SCSS</span>
+                    <span className={ "h3 mr--10 text--textDimmed none md:inline" }>skil3e SCSS</span>
                 </Link>
                 <ThemeChanger className={ "ml--md" }/>
                 <Link className={ "ml--auto mr--md" } to={ "/docs" }>Documentation</Link>
+                <Github/>
 
             </nav>
         </header>
